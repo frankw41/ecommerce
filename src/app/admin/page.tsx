b354409e304d2d9c866d1d25641970a154ce2289
frankw41 <frankwan41@gmail.com>
2024-05-24 08:51:28 +1000

    Add Dashboard page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import db from "@/db/db";
+
+async function getSalesData() {
+	const data = await db.order.aggregate({
+		_sum: { pricePaidInCents: true },
+		_count: true,
+	});
+
+	return {
+		amount: data._sum.pricePaidInCents || 0 / 100,
+		numberOfSales: data._count,
+	};
+}
+
+export default async function AdminDashboard() {
+	const salesData = await getSalesData();
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+			<DashboardCard title="Sales" subtitle="Test" body="body" />
+		</div>
+	);
+}
+
+type DashboardCardProps = {
+	title: string;
+	subtitle: string;
+	body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+				<CardDescription>{subtitle}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>{body}</p>
+			</CardContent>
+		</Card>
+	);
+}
