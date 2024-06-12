f80cc3e464f150d756192ec4307bbc0821d3d37b
frankw41 <frankwan41@gmail.com>
2024-06-12 20:57:07 +1000

    Implement customer home page and products page

---
+++ src/app/(customerFacing)/products/page.tsx
@@ -0,0 +1,38 @@
+import { Suspense } from "react";
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import db from "@/db/db";
+
+function getProducts() {
+	return db.product.findMany({
+		where: { isAvailableForPurchase: true },
+		orderBy: { name: "asc" },
+	});
+}
+
+export default function ProductPage() {
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+			<Suspense
+				fallback={
+					<>
+						<ProductCardSkeleton />
+						<ProductCardSkeleton />
+						<ProductCardSkeleton />
+						<ProductCardSkeleton />
+						<ProductCardSkeleton />
+						<ProductCardSkeleton />
+					</>
+				}
+			>
+				<ProductSuspense />
+			</Suspense>
+		</div>
+	);
+}
+
+async function ProductSuspense() {
+	const products = await getProducts();
+	return products.map((product) => (
+		<ProductCard key={product.id} {...product} />
+	));
+}
